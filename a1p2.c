c7697e484a69b44fb66d5886724198f6a60eac39
Connor Bren <connor.bren@gmail.com>
2025-09-19 14:34:42 -0600

    rename files

---
+++ a1p2.c
@@ -0,0 +1,13 @@
+int is_prime(int num)
+{
+    if (num < 2)
+        return 0;
+    for (int i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+
