52315663f174dfb17da62dd91fa14c175338dec6
Connor Bren <connor.bren@gmail.com>
2025-09-19 21:56:54 -0600

    a1p2, format documents, add gitignore

--- a1p1.c
+++ a1p1.c
@@ -11,16 +11,20 @@
 #define MATRIX_ROW_COUNT 100
 #define MATRIX_COL_COUNT 1000
 
-int find(pid_t *array, int len, pid_t val) {
-    for (int i = 0; i < len; ++i) {
-        if (array[i] == val) {
+int find(pid_t *array, int len, pid_t val)
+{
+    for (int i = 0; i < len; ++i)
+    {
+        if (array[i] == val)
+        {
             return i;
         }
     }
     return -1; // didn't find anything
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int matrix[MATRIX_ROW_COUNT][MATRIX_COL_COUNT]; // Initialize matrix
     memset(matrix, 0, sizeof(matrix));
 
@@ -29,24 +33,27 @@ int main(int argc, char **argv) {
 
     int c;
     bool found = false;
-    while ((c = fgetc(stdin)) != EOF) {
-        switch (c) {
-            case '0':
+    while ((c = fgetc(stdin)) != EOF)
+    {
+        switch (c)
+        {
+        case '0':
             ++col;
             break;
 
-            case '\n': // input files from D2L end with LF so this should suffice
+        case '\n': // input files from D2L end with LF so this should suffice
             ++row;
             col = 0;
             break;
 
-            case ' ':
+        case ' ':
             break;
 
-            case '1': // found the treasure location
+        case '1': // found the treasure location
             found = true;
         }
-        if (found) {
+        if (found)
+        {
             matrix[row][col] = 1; // y = row, x = col
             break;
         }
@@ -54,18 +61,24 @@ int main(int argc, char **argv) {
 
     pid_t pid_row_map[MATRIX_ROW_COUNT];
 
-    for (int i = 0; i < MATRIX_ROW_COUNT; ++i) {
+    for (int i = 0; i < MATRIX_ROW_COUNT; ++i)
+    {
         pid_t pid = fork();
-        if (pid == 0) {
+        if (pid == 0)
+        {
             // Child process
-            for (int j = 0; j < MATRIX_COL_COUNT; ++j) {
-                if (matrix[i][j] == 1) {
+            for (int j = 0; j < MATRIX_COL_COUNT; ++j)
+            {
+                if (matrix[i][j] == 1)
+                {
                     exit(EXIT_SUCCESS);
                 }
             }
             exit(EXIT_FAILURE);
-        } else if (pid < 0) {
-            perror("There was an error creating fork");
+        }
+        else if (pid < 0)
+        {
+            fprintf(stderr, "There was an error creating fork");
             return -1;
         }
         // Parent process
@@ -74,20 +87,25 @@ int main(int argc, char **argv) {
 
     int remaining = MATRIX_ROW_COUNT;
     bool success;
-    while (remaining != 0) {
+    while (remaining != 0)
+    {
         int status;
         pid_t pid = wait(&status);
         int exit_status = WEXITSTATUS(status);
-        if (exit_status == EXIT_SUCCESS) {
+        if (exit_status == EXIT_SUCCESS)
+        {
             int found_row = find(pid_row_map, sizeof(pid_row_map) / sizeof(int), pid);
             int found_column = -1;
-            for (int i = 0; i < MATRIX_COL_COUNT; ++i) {
-                if (matrix[found_row][i] == 1) {
+            for (int i = 0; i < MATRIX_COL_COUNT; ++i)
+            {
+                if (matrix[found_row][i] == 1)
+                {
                     found_column = i;
                 }
             }
-            if (found_column == -1) {
-                perror("Something went wrong, found_column should not be -1! Aborting.");
+            if (found_column == -1)
+            {
+                fprintf(stderr, "Something went wrong, found_column should not be -1! Aborting.");
                 return 1;
             }
             printf("The treasure was found by child with PID %d at row %d, column %d\n", pid, found_row, found_column);
@@ -96,7 +114,8 @@ int main(int argc, char **argv) {
         --remaining;
     }
 
-    if (!success) printf("No treasure was found in the matrix\n");
+    if (!success)
+        printf("No treasure was found in the matrix\n");
 
     return 0;
 }
--- a1p2.c
+++ a1p2.c
@@ -1,3 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <math.h>
+
 int is_prime(int num)
 {
     if (num < 2)
@@ -10,4 +19,86 @@ int is_prime(int num)
     return 1;
 }
 
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: %s LOWER_BOUND UPPER_BOUND N\n", argv[0]);
+        return 1;
+    }
 
+    long lower = atol(argv[1]);
+    long upper = atol(argv[2]);
+    long N_in = atol(argv[3]);
+
+    if (lower > upper || N_in <= 0)
+    {
+        fprintf(stderr, "Invalid arguments.\n");
+        return 1;
+    }
+
+    long total = upper - lower + 1;
+    long M = (N_in > total) ? total : N_in; // final number of children
+    long max_block = (total / M) + (total % M);
+
+    // [counts[M]] [data[M * max_block]]
+    size_t ints_needed = (size_t)M + (size_t)M * (size_t)max_block;
+    size_t shm_bytes = ints_needed * sizeof(int);
+
+    int shmid = shmget(IPC_PRIVATE, shm_bytes, IPC_CREAT | 0666);
+    if (shmid == -1)
+        return 1;
+
+    int *shm = (int *)shmat(shmid, NULL, 0);
+    if (shm == (void *)-1)
+        return 1;
+
+    int *counts = shm;
+    int *data = shm + M;
+
+    for (long i = 0; i < M; ++i)
+        counts[i] = 0;
+
+    for (long i = 0; i < M; ++i)
+    {
+        long start = lower + i * (total / M);
+        long end = (i == M - 1) ? upper : (start + (total / M) - 1);
+
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            // Child: find primes in [start, end], write to its own block
+            int *this_block = data + (i * (long)max_block);
+            int cnt = 0;
+            for (long n = start; n <= end; ++n)
+                if (is_prime((int)n))
+                    this_block[cnt++] = (int)n;
+            counts[i] = cnt;
+            exit(0);
+        }
+        else
+        {
+            printf("Child PID %d checking range [%ld, %ld]\n", (int)pid, start, end);
+        }
+    }
+
+    // Wait for all children
+    for (long i = 0; i < M; ++i)
+        wait(NULL);
+
+    puts("\nParent: All children finished. Primes found:");
+    for (long i = 0; i < M; ++i)
+    {
+        int cnt = counts[i];
+        int *this_block = data + (i * (long)max_block);
+        for (int k = 0; k < cnt; ++k)
+            printf("%d ", this_block[k]);
+    }
+    puts("");
+
+    // Clean up
+    shmdt(shm);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
+}
